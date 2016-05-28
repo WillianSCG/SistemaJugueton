07a22a92a37919461e120dc6a57025f5162f0070
anti-black <anti.bl4ck@gmail.com>
2016-05-27 21:38:14 -0600

    Correcciones en la base de datos
    Adaptación según lo discutido con el Asesor Carlos

--- Jugueton/jugueton.sql
+++ Jugueton/jugueton.sql
@@ -1,17 +1,18 @@
+#create database jugueton;
 
-create table if not exists marcas_producto( #M
+create table if not exists marcas( #M
 	id_marca int(11) unsigned not null auto_increment primary key,
 	marca varchar(35) unique not null,
 	ruta_imagen_marcas varchar(300) not null
 );
 
-create table if not exists categorias_producto( #M
+create table if not exists categorias( #M
 	id_categoria int(11) unsigned not null auto_increment primary key,
 	categoria varchar(35) unique not null,
 	ruta_imagen_categoria varchar(300) not null
 );
 
-create table if not exists clasificaciones_producto( #M
+create table if not exists clasificaciones( #M
 	id_clasificacion int(11) unsigned not null auto_increment primary key,
 	clasificacion varchar(35) unique not null,
 	ruta_imagen_clasificacion varchar(300) not null
@@ -20,7 +21,7 @@ create table if not exists clasificaciones_producto( #M
 create table if not exists productos( #M
 	id_producto int(11) unsigned not null auto_increment primary key,
 	nombre_producto varchar(150) unique not null,
-	descripcion_producto varchar(255) unique not null,
+	descripcion_producto varchar(350) unique not null,
 	existencias_producto int unsigned not null default 0,
 	precio_normal_producto decimal(7,2) unsigned not null,
 	precio_oferta_producto decimal(7,2) unsigned not null,
@@ -30,9 +31,9 @@ create table if not exists productos( #M
 	id_marca int(11) unsigned not null, 
 	estado_oferta enum('false','true') default 'false' not null,
 	index (id_categoria, id_clasificacion, id_marca),
-	foreign key (id_categoria) references jugueton.categorias_producto(id_categoria) on update restrict on delete restrict,
-	foreign key (id_clasificacion) references jugueton.clasificaciones_producto(id_clasificacion) on update restrict on delete restrict,
-	foreign key (id_marca) references jugueton.marcas_producto(id_marca) on update restrict on delete restrict
+	foreign key (id_categoria) references jugueton.categorias(id_categoria) on update restrict on delete restrict,
+	foreign key (id_clasificacion) references jugueton.clasificaciones(id_clasificacion) on update restrict on delete restrict,
+	foreign key (id_marca) references jugueton.marcas(id_marca) on update restrict on delete restrict
 );
 
 create table if not exists imagenes_productos( #M
@@ -55,55 +56,49 @@ create table if not exists promociones( #M
 	foreign key (id_producto) references jugueton.productos(id_producto) on update restrict on delete restrict
 );
 
-#Y
-
-create table if not exists tipos_contactos( #M
-    id_tipos_contacto int(11) unsigned not null auto_increment primary key,
-    tipo_contacto varchar(35) unique not null
+create table if not exists clientes( #I
+    id_cliente int(11) unsigned not null auto_increment primary key,
+	usuario_cliente varchar(25) unique not null,
+	contra_hash_cliente varchar(200) not null,
+	nombres_cliente varchar(100) not null,
+	apellidos_cliente varchar(200) not null,
+	telefono_fijo int(8),
+	telefono_movil int(8),
+	direccion_primaria varchar(250),
+	direccion_secundaria varchar(250),
+	creacion_cliente date not null,
+	ultimo_acceso timestamp not null
 );
 
-create table if not  exists duis( #M
+create table if not exists cuentas_club( #I
+	id_cuenta_club int(11) unsigned not null auto_increment primary key,
+	nombre_cuenta_club varchar(20) unique not null,
+	contra_hash_cuenta_club  varchar(150) not null,
+	creacion_cuenta_club date not null,
+	ultimo_acceso_cuenta_club timestamp not null
+);
+
+create table if not exists duis( #M
 	id_dui int(11) unsigned not null auto_increment primary key,
 	codigo_identificador varchar(20) unique not null,
 	fecha_creacion date not null,
-	esta_activa enum('false', 'true') default 'false' not null
-);
-
-create table if not exists cuentas_compras( #I
-    id_cuenta_compra int(11) unsigned not null auto_increment primary key,
-	nombres_cuenta_compras varchar(100) not null,
-	apellidos_cuenta_compras varchar(200) not null,
-	usuario_cuenta_compras varchar(25) unique not null,
-	contra_hash_cuenta_compras varchar(200) not null,
-	creacion_cuenta_compras date not null,
-	ultimo_acceso timestamp not null,
-	id_dui int(11) unsigned,
-	index (id_dui),
-	foreign key (id_dui) references jugueton.duis(id_dui) on update restrict on delete restrict
-);
-
-create table if not exists contactos_cuentas_compras( #MV
-	id_contacto_cuenta_compra int(11) unsigned not null auto_increment primary key,
-	id_cuenta_compra int(11) unsigned not null,
-	id_tipos_contacto int(11) unsigned not null,
-	valor_contacto varchar(250) not null,
-	creacion_cuenta_compras date not null,
-	ultimo_acceso_cuenta_compras date not null,
-	es_primario enum('false', 'true') default 'true' not null,
-	index(id_tipos_contacto, id_cuenta_compra),
-	foreign key (id_tipos_contacto) references jugueton.tipos_contactos(id_tipos_contacto) on update restrict on delete restrict,
-	foreign key (id_cuenta_compra) references jugueton.cuentas_compras(id_cuenta_compra) on update restrict on delete restrict
+	esta_activa enum('false', 'true') default 'false' not null,
+	id_cliente int(11) unsigned,
+	id_cuenta_club int(11) unsigned,
+	index(id_cliente, id_cuenta_club),
+	foreign key (id_cliente) references jugueton.clientes(id_cliente) on update restrict on delete restrict,
+	foreign key (id_cuenta_club) references jugueton.cuentas_club(id_cuenta_club) on update restrict on delete restrict
 );
 
 create table if not exists ventas( #P
 	id_venta int(11) unsigned not null auto_increment primary key,
-	id_cuenta_compra int(11) unsigned not null,
+	id_cliente int(11) unsigned not null,
 	fecha_venta date not null,
 	cantidad_a_pagar_total decimal(8,2),
 	cantidad_descuento decimal(8,2),
 	cantidad_a_pagar_total_final decimal(8,2),
-	index(id_cuenta_compra), 
-	foreign key (id_cuenta_compra) references jugueton.cuentas_compras(id_cuenta_compra) on update restrict on delete restrict 
+	index(id_cliente), 
+	foreign key (id_cliente) references jugueton.clientes(id_cliente) on update restrict on delete restrict
 );
 
 create table if not exists detalles_ventas(#P
@@ -117,28 +112,31 @@ create table if not exists detalles_ventas(#P
 	foreign key (id_venta) references jugueton.ventas(id_venta)  on update restrict on delete restrict
 );
 
-#Y
-
 create table if not exists descuentos( #MV
-	id_descuento_porcentual int(11) unsigned not null auto_increment primary key,
+	id_descuento int(11) unsigned not null auto_increment primary key,
 	porcentaje int(3) unsigned not null,
 	codigo_identificador char(16) unique not null,
-	usado_cupon enum('false', 'true') default 'false' not null
+	usado_cupon enum('false', 'true') default 'false' not null,
+	creador_id_cuenta_club int(11) unsigned not null,
+	padre_id_cliente int(11) unsigned not null,
+	index(creador_id_cuenta_club, padre_id_cliente),
+	foreign key (creador_id_cuenta_club) references cuentas_club(id_cuenta_club) on update restrict on delete restrict,
+	foreign key (padre_id_cliente) references clientes(id_cliente) on update restrict on delete restrict
 );
 
-create table if not exists cuentas_club( #I
-	id_cuenta_club int(11) unsigned not null auto_increment primary key,
-	nombre_cuenta_club varchar(20) unique not null,
-	contra_hash_cuenta_club  varchar(150) not null,
-	creacion_cuenta_club date not null,
-	ultimo_acceso_cuenta_club timestamp not null
+create table if not exists categorias_juegos( #M
+	id_categoria_juego int(11) unsigned not null auto_increment primary key,
+	categoria_juego varchar(35) unique not null
 );
 
 create table if not exists juegos( #M
 	id_juego int(11) unsigned not null auto_increment primary key,
+	id_categoria_juego int(11) unsigned not null,
 	nombre_juego varchar(50) unique not null,
 	descripcion_juego varchar(250) not null,
-	ruta_juego varchar(150) unique not null
+	ruta_juego varchar(150) unique not null,
+	index(id_categoria_juego),
+	foreign key (id_categoria_juego) references jugueton.categorias_juegos(id_categoria_juego) on update restrict on delete restrict
 );
 
 create table if not exists imagenes_juegos( #M
@@ -150,8 +148,6 @@ create table if not exists imagenes_juegos( #M
 	foreign key (id_juego) references jugueton.juegos(id_juego) on update restrict on delete restrict
 );
 
-#Y
-
 create table if not exists records( #P
 	id_record int(11) unsigned not null auto_increment primary key,
 	id_juego int(11) unsigned not null,
@@ -160,20 +156,6 @@ create table if not exists records( #P
 	index(id_juego, id_cuenta_club),
 	foreign key (id_juego) references jugueton.juegos(id_juego) on update restrict on delete restrict,
 	foreign key (id_cuenta_club) references jugueton.cuentas_club(id_cuenta_club) on update restrict on delete restrict
-);
-
-create table if not exists categorias_juegos( #M
-	id_categoria_juego int(11) unsigned not null auto_increment primary key,
-	categoria_juego varchar(35) unique not null
-);
-
-create table if not exists categorizacion_juegos( #M
-	id_categorizacion_juego int(11) unsigned not null auto_increment primary key,
-	id_juego int(11) unsigned not null,
-	id_categoria int(11) unsigned not null,
-	index(id_juego, id_categoria),
-	foreign key (id_juego) references jugueton.juegos(id_juego) on update restrict on delete restrict,
-	foreign key (id_categoria) references jugueton.categorias_juegos(id_categoria_juego) on update restrict on delete restrict
 );
 
 create table if not exists recompensas( #M
@@ -192,7 +174,7 @@ create table if not exists sucursales( #M
 );
 
 create table if not exists cuentas( #M
-	id_cuenta_mantenimiento int(11) unsigned not null auto_increment primary key,
+	id_cuenta int(11) unsigned not null auto_increment primary key,
 	nombre_completo_usuario varchar(300),
 	dui_usuario varchar(12) unique not null,
 	cuenta_mantenimiento varchar(35) unique not null,
@@ -205,37 +187,34 @@ create table if not exists cuentas( #M
 );
 
 create table if not exists acciones( #M1
-    id_accion int(11) unsigned not null auto_increment primary key,
+    id_accion int(11) unsigned not null primary key,
     accion int(1) not null
 );
 
 create table if not exists pantallas( #M1
     id_pantalla int(11) unsigned not null primary key,
-    nombre_pantalla int(11) unsigned not null
+    nombre_pantalla varchar(35) unique not null
 );
-
 
 create table if not exists permisos( #M1
 	id_permiso int(11) unsigned not null auto_increment primary key,
 	id_accion int(11) unsigned not null,
 	id_pantalla int(11) unsigned not null,
-    id_cuenta_mantenimiento int(11) unsigned not null,
-	index(id_accion, id_pantalla, id_cuenta_mantenimiento),
+    id_cuenta int(11) unsigned not null,
+	index(id_accion, id_pantalla, id_cuenta),
 	foreign key (id_accion) references jugueton.acciones(id_accion) on update restrict on delete restrict,
 	foreign key (id_pantalla) references jugueton.pantallas(id_pantalla) on update restrict on delete restrict,
-	foreign key (id_cuenta_mantenimiento) references jugueton.cuentas(id_cuenta_mantenimiento) on update restrict on delete restrict
+	foreign key (id_cuenta) references jugueton.cuentas(id_cuenta) on update restrict on delete restrict
 );
-
-#Y
 
 create table if not exists listas( #I
 	id_lista int(11) unsigned not null auto_increment primary key,
-	es_carrito enum('false', 'true') default 'false' not null,
+	tipo_lista int(1) unsigned default 0 not null, #0 es normal, 1 es lista de deseos, 2 es carrito
 	nombre_lista varchar(35) not null,
-	id_cuenta_compra int(11) unsigned not null,
+	id_cliente int(11) unsigned not null,
 	creacion_lista date not null,
-	index(id_cuenta_compra),
-	foreign key (id_cuenta_compra) references jugueton.cuentas_compras(id_cuenta_compra) on update restrict on delete restrict
+	index(id_cliente),
+	foreign key (id_cliente) references jugueton.clientes(id_cliente) on update restrict on delete restrict
 );
 
 create table if not exists enlistados( #I
@@ -256,14 +235,14 @@ create table if not exists eventos( #M
 
 create table if not exists noticias( #M
 	id_noticia int(11) unsigned not null auto_increment primary key,
+	cabecera varchar(50) not null,
 	informacion varchar(500) not null,
-	fecha_inicio date not null,
-	fecha_fin date not null
+	fecha timestamp not null
 );
 
 create table if not exists imagenes_slider( #M
 	id_imagen_slider int(11) unsigned not null auto_increment primary key,
 	ruta varchar(150) not null,
 	esta_activo enum('false', 'true') default 'true' not null,
-	slider_padre int(11) unsigned not null //Es como un ID de slider pero no crearemos una tabla de slider por que el usuario no puede crear más sliders.
+	slider_padre int(11) unsigned not null #Es como un ID de slider pero no crearemos una tabla de slider por que el usuario no puede crear más sliders.
 );
